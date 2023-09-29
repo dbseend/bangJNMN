49c08f80f8e5045ec12b7775d94670378910881b
dbseend <shyoon840@gmail.com>
2023-09-29 21:24:11 +0900

    면담 예약 기능 구현

--- bangjnmn/src/components/client/Web_ClientMeet.js
+++ bangjnmn/src/components/client/Web_ClientMeet.js
@@ -1,66 +1,87 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { collection, doc, updateDoc } from "firebase/firestore";
+import { auth, dbService } from "../../api/fbase";
 import { useNavigate } from "react-router-dom";
-import { onReadUserData } from "../../utils/AccountStatus";
 
-const Div = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const Table = styled.table`
+  border-collapse: collapse;
+  border: 1px solid black;
+  width: 200px; /* 원하는 표 너비 설정 */
+`;
+
+const TableCell = styled.td`
+  border: 1px solid black;
+  padding: 8px;
+  text-align: center;
+  cursor: pointer;
 `;
 
 const ClientMeet = () => {
-  const [day, setDay] = useState("");
-  const [selectTime, setSelectTime] = useState("");
-  const [reservedTime, setReservedTime] = useState([]);
-  const [rows, setRows] = useState(Array(10).fill(false));
-  const startTime = 9 * 60; // 09:00를 분 단위로 표현
-  const interval = 30; // 30분 간격
-
+  const [clickedIndex, setClickedIndex] = useState(-1);
+  const [name, setName] = useState("");
+  const [reserved, setReserved] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    onReadUserData(navigate);
-  }, [navigate]);
+  useEffect(() => {}, []);
+  const times = Array.from({ length: 19 }, (_, index) => {
+    const startTime = 9 * 60;
+    const interval = 30;
 
-  const handleClick = (index) => {
-    const newRows = [...rows];
-    newRows[index] = !newRows[index];
-    setRows(newRows);
+    const minutes = startTime + index * interval;
+    const hours = Math.floor(minutes / 60);
+    const minutesOfDay = minutes % 60;
+
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutesOfDay.toString().padStart(2, "0");
+
+    return `${formattedHours}:${formattedMinutes}`;
+  });
+
+  // 클릭 이벤트 핸들러 함수
+  const handleCellClick = (index) => {
+    if (clickedIndex === index) {
+      setClickedIndex(-1);
+    } else {
+      setClickedIndex(index);
+    }
   };
 
-  const changeDay = (e) => {
-    setDay(e.target.value);
+  const reserveMeet = () => {
+    if (reserved == false) {
+      const stuCollection = collection(dbService, "studentUser");
+      const stuRef = doc(stuCollection, "윤성현");
+      updateDoc(stuRef, {
+        meetTime: clickedIndex,
+        meetTF: true,
+      });
+      setReserved(true);
+      console.log(clickedIndex);
+    } else {
+      alert("이미 예약하셨습니다");
+    }
   };
+
   return (
-    <Div>
-      <h1>ClientMeet</h1>
-      <label for="birth">원하는 면담 날짜</label>
-      <table border="1">
+    <div>
+      <Table>
         <tbody>
-          {[...Array(20)].map((_, rowIndex) => {
-            // 각 행에 대한 시간 계산
-            const minutes = startTime + rowIndex * interval;
-            const hours = Math.floor(minutes / 60);
-            const minutesOfDay = minutes % 60;
-
-            // 시간을 2자리 숫자로 표현 (예: 09, 10, 11)
-            const formattedHours = hours.toString().padStart(2, "0");
-            const formattedMinutes = minutesOfDay.toString().padStart(2, "0");
-
-            // 행에 시간을 표시
-            const timeLabel = `${formattedHours}:${formattedMinutes}`;
-
-            return (
-              <tr key={rowIndex}>
-                <td>{timeLabel}</td>
-                {/* 내용이 비어있는 열 */}
-              </tr>
-            );
-          })}
+          {times.map((item, index) => (
+            <tr key={index}>
+              <TableCell
+                style={{
+                  backgroundColor: clickedIndex === index ? "lightblue" : "",
+                }}
+                onClick={() => handleCellClick(index)}
+              >
+                {item}
+              </TableCell>
+            </tr>
+          ))}
         </tbody>
-      </table>
-    </Div>
+      </Table>
+      <button onClick={reserveMeet}>예약하기</button>
+    </div>
   );
 };
 
