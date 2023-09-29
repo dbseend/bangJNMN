98e078f6bce7e4636d6603f704acce24d00f22a3
dbseend <141024382+dbseend@users.noreply.github.com>
2023-09-29 23:57:55 +0900

    Ysh (#20)
    * 로그인 파일 생성
    
    * 참고파일 생성
    
    * 파일 구조 설정
    
    * component로 survey 이동
    
    * 설문 조사 결과 db 저장 연결
    
    * 로그아웃, 로그인 상태 확인 기능 구현
    
    * 필요기능 정리
    
    * 로그인 여부 & 권한 확인 구현
    
    * 팀 검색 필터 및 드롭다운 구현
    
    * 면담 예약 기능 구현
    
    * 넷바 파일 생성
    
    * 페이지 새로고침 시 회원정보 얻어오기

--- bangjnmn/src/components/client/Web_ClientMeet.js
+++ bangjnmn/src/components/client/Web_ClientMeet.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { collection, doc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  updateDoc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 import { auth, dbService } from "../../api/fbase";
 import { useNavigate } from "react-router-dom";
 
 const Table = styled.table`
   border-collapse: collapse;
   border: 1px solid black;
-  width: 200px; /* 원하는 표 너비 설정 */
+  width: 200px;
 `;
 
 const TableCell = styled.td`
@@ -18,12 +24,50 @@ const TableCell = styled.td`
 `;
 
 const ClientMeet = () => {
+  const [allUserData, setAllUserData] = useState("");
   const [clickedIndex, setClickedIndex] = useState(-1);
   const [name, setName] = useState("");
   const [reserved, setReserved] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const checkStatus = async () => {
+      const currentPath = window.location.pathname;
+
+      auth.onAuthStateChanged(async (user) => {
+        if (user) {
+          console.log("로그인 되어있습니다.");
+          setName(user.displayName);
+          const stuRef = doc(dbService, "studentUser", user.displayName);
+          const stuSnap = await getDoc(stuRef);
+          if (stuSnap.exists()) {
+            setReserved(stuSnap.data().meetTF);
+          }
+          if (
+            localStorage.getItem("access") === "client" &&
+            currentPath.includes("admin")
+          ) {
+            console.log("접근할 수 없습니다.");
+            navigate("/client");
+          }
+        } else {
+          console.log("로그인이 필요합니다.");
+          navigate("/");
+        }
+      });
+    };
+
+    const fetchAllData = async () => {
+      const data = await getDocs(collection(dbService, "studentUser"));
+      const newData = data.docs.map((doc) => ({ ...doc.data() }));
+      setAllUserData(newData);
+      console.log(newData);
+    };
+
+    checkStatus();
+    fetchAllData();
+  }, []);
+
   const times = Array.from({ length: 19 }, (_, index) => {
     const startTime = 9 * 60;
     const interval = 30;
@@ -38,7 +82,6 @@ const ClientMeet = () => {
     return `${formattedHours}:${formattedMinutes}`;
   });
 
-  // 클릭 이벤트 핸들러 함수
   const handleCellClick = (index) => {
     if (clickedIndex === index) {
       setClickedIndex(-1);
@@ -57,7 +100,8 @@ const ClientMeet = () => {
       });
       setReserved(true);
       console.log(clickedIndex);
-    } else {
+    }
+    if (reserved == true) {
       alert("이미 예약하셨습니다");
     }
   };
