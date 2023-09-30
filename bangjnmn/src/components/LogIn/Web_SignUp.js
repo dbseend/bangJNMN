9a1d03e86e402c084423ce99a3e6d933ea279c50
KKIMYENN <yenny031003@gmail.com>
2023-10-01 02:45:31 +0900

    Sign up 디자인 (ing)

--- bangjnmn/src/components/LogIn/Web_SignUp.js
+++ bangjnmn/src/components/LogIn/Web_SignUp.js
@@ -5,6 +5,68 @@ import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
 
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  overflow: hidden;
+  background: #38373c;
+  /* width: 100vw; //화면 너비 만큼
+  height: 100vh; */
+/* top: 854px;
+left: 799px; */
+
+`;
+
+const Title = styled.div`
+  margin-top: 85px;
+  color: #f26938;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px; /* 83.333% */
+  letter-spacing: 0.1px;
+  width: 111px;
+  height: 20px;
+  left: 665px;
+
+`;
+
+const Mass1 = styled.div`
+  margin-top: 90px;
+`;
+const Table = styled.div`
+  width: 645px;
+  height: 56px;
+  flex-shrink: 0;
+  border: 1px solid #000;
+  background: #fff;
+`;
+
+const Typo = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: Roboto bold;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px; /* 125% */
+  letter-spacing: 0.1px;
+  top: 201px;
+  left: 416px;
+`;
+const Radio = styled.input`
+  appearance: none;
+  width: 18px;
+  height: 18px;
+  border-radius: 2px;
+  border: 2px solid var(--m-3-sys-light-on-surface-variant, #49454f);
+`;
 const SignUp = () => {
   const [userData, setUserData] = useState("");
   const [name, setName] = useState("");
@@ -173,120 +235,142 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h1>SignUp</h1>
-      <Select
+    <Div>
+      <Title>방주니마니</Title>
+      {/* <Select
         value={selectedOption}
         onChange={handleSelectChange}
         options={options}
         isSearchable
         placeholder="옵션 선택"
-      />
+      /> */}
 
-      {selectedOption && <p>선택한 옵션: {selectedOption.label}</p>}
+      {/* {selectedOption && <p>선택한 옵션: {selectedOption.label}</p>} */}
 
       <form>
-        <p>이름: {userData.displayName}</p>
-        <p>이메일: {userData.email}</p>
-        <br />
-        <label htmlFor="gender">성별</label>
-        <input
-          type="radio"
-          name="gender"
-          value="male"
-          onClick={changeGender}
-        />{" "}
-        남자
-        <input
-          type="radio"
-          name="gender"
-          value="female"
-          onClick={changeGender}
-        />{" "}
-        여자
-        <br />
-        <label htmlFor="studentNumber">학번</label>
-        <input type="text" name="studentNumber" onChange={changeStuNum} />
-        <br />
-        <label htmlFor="major">학부</label>
-        <select value={major} onChange={changeMajor}>
-          <option value="" disabled>
-            학부 선택
-          </option>
-          {majors.map((majorOption) => (
-            <option key={majorOption} value={majorOption}>
-              {majorOption}
-            </option>
-          ))}
-        </select>
-        <br />
-        <label htmlFor="phoneNumber">전화번호</label>
-        <input
-          type="text"
-          name="phoneNumber"
-          onChange={changePhoneNumber}
-          placeholder="전화번호를 입력하세요"
-          required="required"
-          pattern="01[0-9]{9}"
-          maxLength={13}
-        />
-        <br />
-        <label htmlFor="birth"> 생년월일 </label>
-        <input type="date" name="birth" onChange={changeBirth} />
-        <br />
-        <label htmlFor="rc"> RC </label>
-        <select value={rc} onChange={changeRc}>
-          <option value="" disabled>
-            rc 선택
-          </option>
-          {rcs.map((rcOption) => (
-            <option key={rcOption} value={rcOption}>
-              {rcOption}
-            </option>
-          ))}
-        </select>
-        <br />
-        <label htmlFor="team"> 팀 </label>
-        <input type="text" name="team" onChange={changeTeam} />
-        <br />
-        <label htmlFor="dorm"> 호관 </label>
-        <select value={dorm} onChange={changeDorm}>
-          <option value="" disabled>
-            호관 선택
-          </option>
-          {dorms.map((dormOption) => (
-            <option key={dormOption} value={dormOption}>
-              {dormOption}
-            </option>
-          ))}
-        </select>
-        <br />
-        <label htmlFor="roommateNum"> 인실 </label>
-        <input
-          type="radio"
-          name="roommateNum"
-          value="room1"
-          onClick={changeRoommateNum}
-        />{" "}
-        1인실
-        <input
-          type="radio"
-          name="roommateNum"
-          value="room2"
-          onClick={changeRoommateNum}
-        />{" "}
-        2인실
-        <input
-          type="radio"
-          name="roommateNum"
-          value="room4"
-          onClick={changeRoommateNum}
-        />{" "}
-        4인실
+        <Mass1>
+          <Table>
+            <Typo>이름 </Typo> {userData.displayName}{" "}
+          </Table>
+          <Table>
+            <Typo>이메일 </Typo> {userData.email}
+          </Table>
+
+          <Table>
+            <label htmlFor="gender">성별</label>
+            <Radio
+              type="radio"
+              name="gender"
+              value="male"
+              onClick={changeGender}
+            />{" "}
+            남자
+            <Radio
+              type="radio"
+              name="gender"
+              value="female"
+              onClick={changeGender}
+            />{" "}
+            여자
+          </Table>
+          <Table>
+            <label htmlFor="birth"> 생년월일 </label>
+            <input type="date" name="birth" onChange={changeBirth} />
+          </Table>
+
+          <Table>
+            <label htmlFor="phoneNumber">전화번호</label>
+            <input
+              type="text"
+              name="phoneNumber"
+              onChange={changePhoneNumber}
+              placeholder="전화번호를 입력하세요"
+              required="required"
+              pattern="01[0-9]{9}"
+              maxLength={13}
+            />
+          </Table>
+        </Mass1>
+        
+        {/* <Mass> */}
+          <Table>
+            <label htmlFor="studentNumber">학번</label>
+            <input type="text" name="studentNumber" onChange={changeStuNum} />
+          </Table>
+          <Table>
+            <label htmlFor="major">학부</label>
+            <select value={major} onChange={changeMajor}>
+              <option value="" disabled>
+                학부 선택
+              </option>
+              {majors.map((majorOption) => (
+                <option key={majorOption} value={majorOption}>
+                  {majorOption}
+                </option>
+              ))}
+            </select>
+          </Table>
+
+          <Table>
+            <label htmlFor="rc"> RC </label>
+            <select value={rc} onChange={changeRc}>
+              <option value="" disabled>
+                rc 선택
+              </option>
+              {rcs.map((rcOption) => (
+                <option key={rcOption} value={rcOption}>
+                  {rcOption}
+                </option>
+              ))}
+            </select>
+          </Table>
+
+          <Table>
+            <label htmlFor="dorm"> 호관 </label>
+            <select value={dorm} onChange={changeDorm}>
+              <option value="" disabled>
+                호관 선택
+              </option>
+              {dorms.map((dormOption) => (
+                <option key={dormOption} value={dormOption}>
+                  {dormOption}
+                </option>
+              ))}
+            </select>
+          </Table>
+          <Table>
+            <label htmlFor="team"> 팀 </label>
+            <input type="text" name="team" onChange={changeTeam} />
+          </Table>
+          <Table>
+            <label htmlFor="roommateNum"> 인실 </label>
+            <Radio
+              type="radio"
+              name="roommateNum"
+              value="room4"
+              onClick={changeRoommateNum}
+            />{" "}
+            4인실
+            <Radio
+              type="radio"
+              name="roommateNum"
+              value="room2"
+              onClick={changeRoommateNum}
+            />{" "}
+            2인실
+            <Radio
+              type="radio"
+              name="roommateNum"
+              value="room1"
+              onClick={changeRoommateNum}
+            />{" "}
+            1인실
+          </Table>
+        {/* </Mass> */}
         <br />
       </form>
       <button onClick={signUp}> 회원가입 </button>
-    </div>
+    </Div>
   );
 };
 
