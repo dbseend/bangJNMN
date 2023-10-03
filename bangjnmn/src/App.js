d0fe8ab67721c219319d7c2e46769ab88e8cd044
dbseend <141024382+dbseend@users.noreply.github.com>
2023-10-04 03:09:30 +0900

    Ysh (#33)
    * 로그인 파일 생성
    
    * 참고파일 생성
    
    * 파일 구조 설정
    
    * component로 survey 이동
    
    * 설문 조사 결과 db 저장 연결
    
    * 로그아웃, 로그인 상태 확인 기능 구현
    
    * 필요기능 정리
    
    * 로그인 여부 & 권한 확인 구현
    
    * 팀 검색 필터 및 드롭다운 구현
    
    * 면담 예약 기능 구현
    
    * 넷바 파일 생성
    
    * 페이지 새로고침 시 회원정보 얻어오기
    
    * 로그인 버트 디자인:
    
    * 면담 예약 db 수정 중
    
    * 마이페이지 전화번호 변경 오류 해결
    
    * 면담 기능 구현
    
    * .

--- bangjnmn/src/App.js
+++ bangjnmn/src/App.js
@@ -8,7 +8,7 @@ import AdminSurvey from "./pages/admin/AdminSurvey";
 import ClientHome from "./pages/client/ClientHome";
 import ClientSurvey from "./pages/client/ClientSurvey";
 import ClientMeet from "./pages/client/ClientMeet";
-import ClientMyPage from "./components/client/Web_ClientMyPage";
+import ClientMyPage from "./pages/client/ClientMyPage";
 
 function App() {
   return (
@@ -28,9 +28,9 @@ function AdminRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AdminHome />} />
-      <Route path="survey" element={<AdminSurvey />} />
-      <Route path="meet" element={<AdminMeet />} />
-      <Route path="room" element={<AdminRoom />} />
+      <Route path="/survey" element={<AdminSurvey />} />
+      <Route path="/meet" element={<AdminMeet />} />
+      <Route path="/room" element={<AdminRoom />} />
     </Routes>
   );
 }
@@ -40,7 +40,7 @@ function ClientRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ClientHome />} />
-      <Route path="survey" element={<ClientSurvey />} />
+      <Route path="/survey" element={<ClientSurvey />} />
       <Route path="/meet" element={<ClientMeet />} />
       <Route path="/mypage" element={<ClientMyPage />} />
     </Routes>
