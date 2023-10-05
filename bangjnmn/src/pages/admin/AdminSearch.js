ccb5c0e48e7d6079d2209b750df07f3fa1855147
dbseend <shyoon840@gmail.com>
2023-10-05 23:09:16 +0900

    오류 수정

--- bangjnmn/src/pages/admin/AdminSearch.js
+++ bangjnmn/src/pages/admin/AdminSearch.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
-import Web_AdminSearch from '../../components/admin/Web_AdminSearch';
+import Web_AdminSearch from "../../components/admin/Web_AdminSearch";
 
 import { styled } from "styled-components";
 
 const Div = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center; 
-  justify-content: center; 
+  align-items: center;
+  justify-content: center;
   margin: 0 auto;
   width: 100%;
   overflow: hidden;
-`
+`;
 
-const [isModalOpen, setIsModalOpen] = useState(false);
+const AdminSearch = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const options = ["Option 1", "Option 2", "Option 3"];
 
   const handleButtonClick = () => {
     setIsModalOpen(true);
@@ -23,41 +26,32 @@ const [isModalOpen, setIsModalOpen] = useState(false);
     setIsModalOpen(false);
   };
 
-const AdminSearch = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const options = ['Option 1', 'Option 2', 'Option 3'];
+  return (
+    <Div>
+      <h1>AdminSearch</h1>
+      <div>
+        <span>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? "Close menu" : "Open menu"}
+          </button>
 
-    return (
-        <Div>
-            <h1>AdminSearch</h1>
-            <div>
-              <span>
-              <button onClick={() => setIsOpen(!isOpen)}>
-                {isOpen ? 'Close menu' : 'Open menu'}
-              </button>
-
-              {isOpen && (
-              <ul>
-                {options.map((option, index) => (
-                  <li key={index}>{option}</li>
-                ))}
-              </ul>
-              )}
-              </span>
-              <div>
-                테이블 띄우기
-              </div>
-              {/* 버튼 */}
-              <button onClick={handleButtonClick}>모달 열기</button>
-
-              {/* 모달 */}
-              <Web_AdminSearch isOpen={isModalOpen} onClose={handleCloseModal} />
-              클릭시 모달창 띄우기
-            </div>
-            
-
-        </Div>
-    )
-}
+          {isOpen && (
+            <ul>
+              {options.map((option, index) => (
+                <li key={index}>{option}</li>
+              ))}
+            </ul>
+          )}
+        </span>
+        <div>테이블 띄우기</div>
+        {/* 버튼 */}
+        <button onClick={handleButtonClick}>모달 열기</button>
+        {/* 모달 */}
+        <Web_AdminSearch isOpen={isModalOpen} onClose={handleCloseModal} />
+        클릭시 모달창 띄우기
+      </div>
+    </Div>
+  );
+};
 
 export default AdminSearch;
