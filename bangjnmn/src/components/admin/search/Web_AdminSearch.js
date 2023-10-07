85044294382caf5558e13a86f503cacae7f85d71
dirhq88 <dirhq88@gmail.com>
2023-10-07 13:16:01 +0900

    AdminSearch기능 구현함(filter 검색 & 상세페이지 이동)

--- bangjnmn/src/components/admin/search/Web_AdminSearch.js
+++ bangjnmn/src/components/admin/search/Web_AdminSearch.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from "react";
 import  styled  from "styled-components";
 import Select from 'react-select';
 import { doc, getDoc, getDocs, setDoc, collection } from "firebase/firestore";
-import { auth, dbService } from "../../api/fbase";
+import { auth, dbService } from "../../../api/fbase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
-
+import Item from "./Web_Item";
 
 
 
@@ -47,48 +46,51 @@ const Search = styled.div`
   align-items: center;
   justify-content: center;
   width: 30%;
-  margin-bottom: 30%;
+  margin-bottom: 10%;
 `
 
-function DataList() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-      const fetchData = async () => {
-          const dataCollection = await dbService.collection('collectionName').get();
-          setData(dataCollection.docs.map(doc => doc.data()));
-      };
-      fetchData();
-  }, []);
-
-  return (
-    <ul>
-        {data.map((item, index) => (
-            <li key={index}>
-                {item.fieldName}
-            </li>
-        ))}
-    </ul>
-);
-}
 
 
 const Web_AdminSearch = () => {
   
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]); // 새로운 상태 변수
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const dataCollection = await dbService.collection('collectionName').get();
-            setData(dataCollection.docs.map(doc => doc.data()));
-        };
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const dataCollection = await dbService.collection('user').get();
+    //         setData(dataCollection.docs.map(doc => doc.data()));
+    //     };
+    //     fetchData();
+    // }, []);
+  const userCollection = collection(dbService, "user")
+
+  useEffect(() => {
+    async function getUsers() {
+      const data = await getDocs(userCollection)
+      console.log(data)
+      setData(data.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+    }
+    getUsers()
+  }, [data.length])
+
+  useEffect(() => {
+    if (selectedOption && searchTerm) {
+      // 선택된 옵션과 검색어에 따라 데이터 필터링
+      const filteredUsers = data.filter((user) =>
+        user[selectedOption].includes(searchTerm)
+      );
+      setFilteredData(filteredUsers);
+    } else {
+      // 선택된 옵션이 없거나 검색어가 없으면 모든 데이터 표시
+      setFilteredData(data);
+    }
+  }, [selectedOption, searchTerm]);
 
   const options = [
-    { value: 'name', label: 'Name' },
-    { value: 'studentId', label: 'Student ID' },
-    { value: 'team', label: 'Team' }
+    { value: 'name', label: '이름' },
+    { value: 'stuNum', label: '학번' },
+    { value: 'team', label: '팀' }
   ];
   const [isModalOpen, setIsModalOpen] = useState(false);
   //const options = ["Option 1", "Option 2", "Option 3"];
@@ -109,16 +111,18 @@ const Web_AdminSearch = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    
-    // 전달된 콜백 함수 호출하여 실제로 검색 작업 수행
-    //onSearch(searchTerm);
-    
-    // 필요한 로직 추가
+  const handleSearchSubmit = () => {
 
-    // 예시: 폼 제출 후에는 입력값 초기화
-    setSearchTerm('');
+    if (selectedOption && searchTerm) {
+      // 선택된 옵션과 검색어에 따라 데이터 필터링
+      const filteredUsers = data.filter((user) =>
+        user[selectedOption].toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filteredUsers);
+    } else {
+      // 선택된 옵션이 없거나 검색어가 없으면 모든 데이터 표시
+      setFilteredData(data);
+    }
   };
 
   return (
@@ -127,9 +131,10 @@ const Web_AdminSearch = () => {
         <Search>
           <Select 
               options={options} 
-              onChange={setSelectedOption}
+              onChange={(option) => setSelectedOption(option.value)}
               placeholder="Select a search type"
           />
+          
           <input
               style={{ marginLeft: '8px' }}
               type="text"
@@ -137,20 +142,31 @@ const Web_AdminSearch = () => {
               onChange={handleInputChange}
               placeholder="검색어 입력"
           />
-          <button type="submit" style={{ marginLeft: '8px' }}>
+          <button type="submit" style={{ marginLeft: '8px' }} onClick={handleSearchSubmit}>
           🔍
           </button>
         </Search>
 
         {/* Data list */}
-        <ul>
-                {data.map((item,index) => (
-                    <li key={index}>
-                        {"이름"}
-                        {item.name}
-                    </li>
-                ))}
-            </ul>
+      <div>
+      <table>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>학번</th>
+            <th>팀</th>
+            <th>학부</th>
+            <th>생년월일</th>
+            <th>호실</th>
+          </tr>
+        </thead>
+        <tbody>
+        {filteredData.map((user, idx) => {   // change 'data' to 'filteredData'
+                return <Item key={idx} data={user} />
+              })}
+        </tbody>
+      </table>
+    </div>
       
         {/* <div>테이블 띄우기</div>
           
--- bangjnmn/src/pages/admin/AdminSearch.js
+++ bangjnmn/src/pages/admin/AdminSearch.js
@@ -1,67 +1,5 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import Web_AdminSearch from '../../components/admin/Web_AdminSearch';
-
-import { styled } from "styled-components";
-
-const Div = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center; 
-  justify-content: center; 
-  margin: 0 auto;
-  width: 100%;
-  overflow: hidden;
-`
-
-const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleButtonClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-const AdminSearch = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const options = ['Option 1', 'Option 2', 'Option 3'];
-
-    return (
-        <Div>
-            <h1>AdminSearch</h1>
-            <div>
-              <span>
-              <button onClick={() => setIsOpen(!isOpen)}>
-                {isOpen ? 'Close menu' : 'Open menu'}
-              </button>
-
-              {isOpen && (
-              <ul>
-                {options.map((option, index) => (
-                  <li key={index}>{option}</li>
-                ))}
-              </ul>
-              )}
-              </span>
-              <div>
-                테이블 띄우기
-              </div>
-              {/* 버튼 */}
-              <button onClick={handleButtonClick}>모달 열기</button>
-
-              {/* 모달 */}
-              <Web_AdminSearch isOpen={isModalOpen} onClose={handleCloseModal} />
-              클릭시 모달창 띄우기
-            </div>
-            
-
-        </Div>
-    )
-}
-=======
-import Web_AdminSearch from "../../components/admin/Web_AdminSearch";
+import Web_AdminSearch from "../../components/admin/search/Web_AdminSearch";
 
 const AdminSearch = () => {
   return (
@@ -70,6 +8,5 @@ const AdminSearch = () => {
     </div>
   );
 };
->>>>>>> main
 
 export default AdminSearch;
