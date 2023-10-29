717064460387134c16c8b88f3e2900cdabd0c3af
dbseend <141024382+dbseend@users.noreply.github.com>
2023-10-30 03:21:35 +0900

    Ysh (#66)
    * 로그인 파일 생성
    
    * 참고파일 생성
    
    * 파일 구조 설정
    
    * component로 survey 이동
    
    * 설문 조사 결과 db 저장 연결
    
    * 로그아웃, 로그인 상태 확인 기능 구현
    
    * 필요기능 정리
    
    * 로그인 여부 & 권한 확인 구현
    
    * 팀 검색 필터 및 드롭다운 구현
    
    * 면담 예약 기능 구현
    
    * 넷바 파일 생성
    
    * 페이지 새로고침 시 회원정보 얻어오기
    
    * 로그인 버트 디자인:
    
    * 면담 예약 db 수정 중
    
    * 마이페이지 전화번호 변경 오류 해결
    
    * 면담 기능 구현
    
    * .
    
    * 관리자 면담:
    
    * 면담
    
    * 오류 수정
    
    * 면담 수정
    
    * db 연결 수정
    
    * 면담 수정
    
    * 병합 수정
    
    * 면담 취소 시간 설정
    
    * 권한 로컬 스토리지 저장 및 시간 설정
    
    * .
    
    * 학생 면담 예약 삭제 및 예약 확인 오류 수정
    
    * 로그인/회원가입 수정
    
    * remove firebase, filter files
    
    * 간사님 면담 페이지 학생 이름 수정
    
    * 파일 이동 규칙 처리
    
    ---------
    
    Co-authored-by: KKIMYENN <yenny031003@gmail.com>

--- bangjnmn/src/components/admin/search/Web_AdminSearch.js
+++ bangjnmn/src/components/admin/search/Web_AdminSearch.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react";
-import  styled  from "styled-components";
-import Select from 'react-select';
-import { doc, getDoc, getDocs, setDoc, collection } from "firebase/firestore";
-import { auth, dbService } from "../../../api/fbase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { collection, doc, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import styled from "styled-components";
+import { dbService } from "../../../api/fbase";
+import { checkStatus } from "../../../utils/CheckStatus";
 import Item from "./Web_Item";
-
 
 const Div = styled.div`
   display: flex;
@@ -25,32 +23,30 @@ const Search = styled.div`
   justify-content: center;
   width: 30%;
   margin-bottom: 10%;
-`
-
-
+`;
 
 const Web_AdminSearch = () => {
-  
+  const [user, setUser] = useState();
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]); // 새로운 상태 변수
   const [selectedOption, setSelectedOption] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
-  const userCollection = collection(dbService, "user")
+  const [searchTerm, setSearchTerm] = useState("");
+  const userCollection = collection(dbService, "user");
 
   useEffect(() => {
+    checkStatus(setUser);
     async function getUsers() {
-      const data = await getDocs(userCollection)
-      console.log(doc)
-      setData(data.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+      const data = await getDocs(userCollection);
+      console.log(doc);
+      setData(data.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
     }
-    getUsers()
-  }, [data.length])
-
+    getUsers();
+  }, [data.length]);
 
   const options = [
-    { value: 'name', label: '이름' },
-    { value: 'stuNum', label: '학번' },
-    { value: 'team', label: '팀' }
+    { value: "name", label: "이름" },
+    { value: "stuNum", label: "학번" },
+    { value: "team", label: "팀" },
   ];
 
   const handleInputChange = (event) => {
@@ -58,14 +54,15 @@ const Web_AdminSearch = () => {
   };
 
   const handleSearchSubmit = () => {
-
     if (selectedOption && searchTerm) {
       let filteredUsers;
 
-      filteredUsers = data.filter((user) =>
-      user[selectedOption] && user[selectedOption].toLowerCase().includes(searchTerm.toLowerCase())
+      filteredUsers = data.filter(
+        (user) =>
+          user[selectedOption] &&
+          user[selectedOption].toLowerCase().includes(searchTerm.toLowerCase())
       );
-      
+
       setFilteredData(filteredUsers);
     } else {
       // 선택된 옵션이 없거나 검색어가 없으면 필터링된 데이터를 초기화
@@ -74,50 +71,52 @@ const Web_AdminSearch = () => {
   };
 
   return (
-    <Div> 
+    <Div>
       <h1>AdminSearch</h1>
-        <Search>
-          <Select 
-              options={options} 
-              onChange={(option) => setSelectedOption(option.value)}
-              placeholder="Select a search type"
-          />
-          
-          <input
-              style={{ marginLeft: '8px' }}
-              type="text"
-              value={searchTerm}
-              onChange={handleInputChange}
-              placeholder="검색어 입력"
-          />
-          <button type="submit" style={{ marginLeft: '8px' }} onClick={handleSearchSubmit}>
+      <Search>
+        <Select
+          options={options}
+          onChange={(option) => setSelectedOption(option.value)}
+          placeholder="Select a search type"
+        />
+
+        <input
+          style={{ marginLeft: "8px" }}
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          placeholder="검색어 입력"
+        />
+        <button
+          type="submit"
+          style={{ marginLeft: "8px" }}
+          onClick={handleSearchSubmit}
+        >
           🔍
-          </button>
-        </Search>
+        </button>
+      </Search>
 
       <div>
-      <table>
-        <thead>
-          <tr>
-            <th>이름</th>
-            <th>학번</th>
-            <th>팀</th>
-            <th>학부</th>
-            <th>생년월일</th>
-            <th>호실</th>
-          </tr>
-        </thead>
-        <tbody>
-        {filteredData.map((user, idx) => {   // change 'data' to 'filteredData'
-                return <Item key={idx} data={user} />
-              })}
-        </tbody>
-      </table>
-    </div>
-      
+        <table>
+          <thead>
+            <tr>
+              <th>이름</th>
+              <th>학번</th>
+              <th>팀</th>
+              <th>학부</th>
+              <th>생년월일</th>
+              <th>호실</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((user, idx) => {
+              // change 'data' to 'filteredData'
+              return <Item key={idx} data={user} />;
+            })}
+          </tbody>
+        </table>
+      </div>
     </Div>
-
-    
   );
 };
 
