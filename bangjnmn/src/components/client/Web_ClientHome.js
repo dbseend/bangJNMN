7532733e098718c09184183cca882a4e0db175d4
KKIMYENN <yenny031003@gmail.com>
2023-11-03 00:21:52 +0900

    디자인 수정

--- bangjnmn/src/components/client/Web_ClientHome.js
+++ bangjnmn/src/components/client/Web_ClientHome.js
@@ -1,7 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { checkStatus } from "../../utils/CheckStatus";
+import styled from "styled-components";
+import main from "../../assets/img/main.png";
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin : 0;
+    padding: 0;
+  }
+  `;
+
+const Div = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  height: 100vh;
+  overflow: hidden;
+`;
+const MainImg = styled.img`
+width: 1317px;
+/* height: 963px; */
+
+`
 const ClientHome = () => {
   const [user, setUser] = useState("");
   const navigate = useNavigate();
@@ -11,9 +35,10 @@ const ClientHome = () => {
   }, []);
 
   return (
-    <div>
-      <h1>ClientHome</h1>
-    </div>
+    <Div>
+      <GlobalStyle/>
+      <MainImg src = {main}/>
+    </Div>
   );
 };
 
--- bangjnmn/src/components/client/Web_ClientNavBar.js
+++ bangjnmn/src/components/client/Web_ClientNavBar.js
@@ -2,25 +2,30 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../api/fbase";
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin : 0;
+    padding: 0;
+  }
+  `;
 const Div = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: center;
   align-items: center; 
-  justify-content: center; 
-  margin: -8px;
-  width: Fixed (1,440px);
+  /* justify-content: center;  */
   overflow: hidden;
   display: flex;
   height: 30px;
-  padding: 16px 80px;
   gap: 48px;
-
+  height: 72px;
   border-bottom: 1px solid var(--cool-gray-20, #DDE1E6);
-  background: #204E4A;
+  background: #04589C;
 `;
 const Home = styled.div`
-width: 111px;
+/* width: 111px; */
+
 height: 20px;
 font-family: Roboto;
 font-size: 24px;
@@ -29,14 +34,16 @@ line-height: 20px;
 letter-spacing: 0.10000000149011612px;
 text-align: center;
 color: #fff9f3;
-margin-right:532px;
+margin-left:50px;
+margin-right:770px;
 margin-top:16px;
 margin-bottom: 16px;
 `
 const Menu = styled.div`
-width: Fixed (331px);
+/* width: Fixed (331px); */
 height: Hug (48px);
 gap: 16px;
+cursor: pointer;
 
 //styleName: Other/Menu M;
 font-family: Roboto;
@@ -46,7 +53,7 @@ line-height: 16px;
 letter-spacing: 0em;
 text-align: left;
 color: #fff9f3;
-
+text-decoration: ${(props) => (props.isSelected ? "underline" : "none")};
 `
 
 // const Logout = styled.button`
@@ -67,59 +74,59 @@ color: #fff9f3;
 // gap: 10px;
 // `
 
-const Logout = styled.button`
-width: 105px;
-height: 40px;
-border-radius: 28px;
-border: 1px solid #DDE1E6;
-background-color: #CECCCC;
-font-family: Roboto;
-font-size: 16px;
-font-weight: 500;
-line-height: 16px;
-letter-spacing: 0.5px;
-color: #FFFFFF;
-text-align: center;
+const Logout = styled.div`
+height: Hug (48px);
+gap: 16px;
+cursor: pointer;
 
-padding: 0px, 16px, 0px, 16px;
-gap: 10px;
-`
-const Merong1 = styled.div`
-width: 105px;
-height: hug 40px;
-cursor: pointer;
+//styleName: Other/Menu M;
 font-family: Roboto;
 font-size: 16px;
 font-weight: 500;
 line-height: 16px;
 letter-spacing: 0em;
-text-align: center;
+text-align: left;
 color: #fff9f3;
+
+/* padding: 0px, 16px, 0px, 16px;
+gap: 10px; */
 `
-const Merong2 = styled.div`
-width: 105px;
-height: hug 40px;
-cursor: pointer;
-font-family: Roboto;
-font-size: 16px;
-font-weight: 500;
-line-height: 16px;
-letter-spacing: 0em;
-text-align: center;
-color: #fff9f3;
-`
-const Merong3 = styled.div`
-width: 105px;
-height: hug 40px;
-cursor: pointer;
-font-family: Roboto;
-font-size: 16px;
-font-weight: 500;
-line-height: 16px;
-letter-spacing: 0em;
-text-align: center;
-color: #fff9f3;
-`
+// const Merong1 = styled.div`
+// width: 105px;
+// height: hug 40px;
+// cursor: pointer;
+// font-family: Roboto;
+// font-size: 16px;
+// font-weight: 500;
+// line-height: 16px;
+// letter-spacing: 0em;
+// text-align: center;
+// color: #fff9f3;
+// `
+// const Merong2 = styled.div`
+// width: 105px;
+// height: hug 40px;
+// cursor: pointer;
+// font-family: Roboto;
+// font-size: 16px;
+// font-weight: 500;
+// line-height: 16px;
+// letter-spacing: 0em;
+// text-align: center;
+// color: #fff9f3;
+// `
+// const Merong3 = styled.div`
+// width: 105px;
+// height: hug 40px;
+// cursor: pointer;
+// font-family: Roboto;
+// font-size: 16px;
+// font-weight: 500;
+// line-height: 16px;
+// letter-spacing: 0em;
+// text-align: center;
+// color: #fff9f3;
+// `
 const Web_clientNavBar = () => {
   const navigate = useNavigate();
 
@@ -144,12 +151,12 @@ const Web_clientNavBar = () => {
   };
   return (
     <Div>
+      <GlobalStyle/>
         <Home onClick={moveToHome}>방주니마니</Home>
       {/* <Margin></Margin> */}
         <Menu onClick={moveToSurvey}>설문하기</Menu>
         <Menu onClick={moveToMeet}>면담 예약하기</Menu>
         <Menu onClick={moveToMyPage}>마이페이지</Menu>
-        <Menu></Menu> <Menu></Menu>
           <Logout onClick={onLogOutClick}> 로그아웃</Logout>
     </Div>
   );
