57b172c95bf2ac2cf4e4931b3c4b26ad4b9968f9
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2023-11-04 06:18:18 +0900

    Kye (#98)
    * 윤성현 적음
    
    * 와 윤성현 사고쳤다 미친...
    
    * 로그인 파일 생성
    
    * 참고파일 생성
    
    * 하아..
    
    * 파일 구조 설정
    
    * 팀, 파이어베이스 연동 제외 간단한 기능 구현함 (수정 필요)
    
    * component로 survey 이동
    
    * 설문 조사 결과 db 저장 연결
    
    * 로그아웃, 로그인 상태 확인 기능 구현
    
    * 필요기능 정리
    
    * 로그인 여부 & 권한 확인 구현
    
    * 팀 검색 필터 및 드롭다운 구현
    
    * 면담 예약 기능 구현
    
    * 넷바 파일 생성
    
    * 페이지 새로고침 시 회원정보 얻어오기
    
    * 로그인 버트 디자인:
    
    * 너무 혼란스러워
    
    * Sign up 디자인 (ing)
    
    * 면담 예약 db 수정 중
    
    * 업데이트
    
    * 마이페이지 전화번호 변경 오류 해결
    
    * 면담 기능 구현
    
    * .
    
    * 관리자 면담:
    
    * 면담
    
    * 오류 수정
    
    * 면담 수정
    
    * 업데이트 중
    
    * db 연결 수정
    
    * 유효성 검사 외 전부 업데이트
    
    * 면담 수정
    
    * 병합 수정
    
    * 면담 취소 시간 설정
    
    * 권한 로컬 스토리지 저장 및 시간 설정
    
    * 한동 메일 아닐 시 첫화면으로 돌아오게 수정
    
    * 기타 수정 사항 반영
    
    * .
    
    * 학생 면담 예약 삭제 및 예약 확인 오류 수정
    
    * 로그인/회원가입 수정
    
    * login page
    
    * 수정 중
    
    * 오류 수정 중
    
    * 회원가입 디자인
    
    * 디자인 수정
    
    * 아직 디자인 수정 중
    
    * 면담 디자인 중
    
    * 홈, 넷바 디자인, 박스 위치 수정
    
    * 관리자 넷바, 면담 디자인
    
    ---------
    
    Co-authored-by: dbseend <shyoon840@gmail.com>
    Co-authored-by: dbseend <141024382+dbseend@users.noreply.github.com>

--- bangjnmn/src/components/admin/Web_AdminHome.js
+++ bangjnmn/src/components/admin/Web_AdminHome.js
@@ -1,6 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { checkStatus } from "../../utils/CheckStatus";
+import styled from "styled-components";
+import main from "../../assets/img/main.png";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin : 0;
+    padding: 0;
+  }
+  `;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  overflow: hidden;
+`;
+const MainImg = styled.img`
+  width: 1500px;
+  height: 900px;
+`;
 
 const AdminHome = () => {
   const [user, setUser] = useState("");
@@ -9,11 +32,33 @@ const AdminHome = () => {
   useEffect(() => {
     checkStatus(setUser);
   }, []);
+
   return (
-    <div>
-      <h1>AdminHome</h1>
-    </div>
+    <Div>
+      <GlobalStyle />
+      <MainImg src={main} draggable="false" />
+    </Div>
   );
 };
 
 export default AdminHome;
+
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { checkStatus } from "../../utils/CheckStatus";
+
+// const AdminHome = () => {
+//   const [user, setUser] = useState("");
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     checkStatus(setUser);
+//   }, []);
+//   return (
+//     <div>
+//       <h1>AdminHome</h1>
+//     </div>
+//   );
+// };
+
+// export default AdminHome;
--- bangjnmn/src/components/client/Web_ClientNavBar.js
+++ bangjnmn/src/components/client/Web_ClientNavBar.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../api/fbase";
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,66 +14,64 @@ const Div = styled.div`
   display: flex;
   justify-content: space-between;
   flex-direction: center;
-  align-items: center; 
+  align-items: center;
   overflow: hidden;
   display: flex;
   height: 30px;
   gap: 48px;
   height: 72px;
-  background: #04589C;
+  background: #04589c;
   padding: 0 50px;
 `;
 
 const Home = styled.div`
-height: 20px;
-font-family: Roboto;
-font-size: 24px;
-font-weight: 700;
-line-height: 20px;
-letter-spacing: 0.10000000149011612px;
-text-align: center;
-color: #fff9f3;
-/* margin-left:50px; */
-margin-right:770px;
-margin-top:16px;
-margin-bottom: 16px;
-cursor:pointer;
-`
+  height: 20px;
+  font-family: Roboto;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: 0.10000000149011612px;
+  text-align: center;
+  color: #fff9f3;
+  /* margin-left:50px; */
+  margin-right: 770px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  cursor: pointer;
+`;
 const Container = styled.div`
-display: flex;
+  display: flex;
   gap: 16px;
-
-`
+`;
 const Menu = styled.div`
-/* width: Fixed (331px); */
-height: Hug (48px);
-gap: 16px;
-cursor: pointer;
+  /* width: Fixed (331px); */
+  height: Hug (48px);
+  gap: 16px;
+  cursor: pointer;
 
-font-family: Roboto;
-font-size: 16px;
-font-weight: 500;
-line-height: 16px;
-letter-spacing: 0em;
-margin-right: 30px;
-color: #fff9f3;
-
-`
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 0em;
+  margin-right: 30px;
+  color: #fff9f3;
+`;
 
 const Logout = styled.div`
-height: Hug (48px);
-gap: 16px;
-cursor: pointer;
+  height: Hug (48px);
+  gap: 16px;
+  cursor: pointer;
 
-//styleName: Other/Menu M;
-font-family: Roboto;
-font-size: 16px;
-font-weight: 500;
-line-height: 16px;
-letter-spacing: 0em;
-text-align: left;
-color: #fff9f3;
-`
+  //styleName: Other/Menu M;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #fff9f3;
+`;
 const Web_clientNavBar = () => {
   const navigate = useNavigate();
 
@@ -98,15 +96,14 @@ const Web_clientNavBar = () => {
   };
   return (
     <Div>
-      <GlobalStyle/>
-        <Home onClick={moveToHome}>방주니마니</Home>
-        <Container>
+      <GlobalStyle />
+      <Home onClick={moveToHome}>방주니마니</Home>
+      <Container>
         <Menu onClick={moveToSurvey}>설문하기</Menu>
         <Menu onClick={moveToMeet}>면담 예약하기</Menu>
         <Menu onClick={moveToMyPage}>마이페이지</Menu>
-          <Logout onClick={onLogOutClick}> 로그아웃</Logout>
-        </Container>
-        
+        <Logout onClick={onLogOutClick}> 로그아웃</Logout>
+      </Container>
     </Div>
   );
 };
