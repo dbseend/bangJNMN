5c65b2cbca0fabef4334a5992e2175157acb5c72
dirhq88 <126394721+dirhq88@users.noreply.github.com>
2023-11-04 18:14:19 +0900

    Dirhq (#102)
    * No change
    
    * Router 변경 및 AdminSearch.js 생성
    
    * change
    
    * change
    
    * AdminSearch기능 구현함(filter 검색 & 상세페이지 이동)
    
    * change Web Item
    
    * debug
    
    * change Web_Index.js(상세페이지)
    
    * debug
    
    * 디자인 수정완료

--- bangjnmn/src/components/admin/search/Web_Item.js
+++ bangjnmn/src/components/admin/search/Web_Item.js
@@ -1,31 +1,96 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  styled  from "styled-components";
+
+const TableRow = styled.tr`
+  border: 1px solid #000;
+`;
+
+const TableCell = styled.td`
+  padding: 8px;
+`;
+
+const Divh = styled.td`
+  border-bottom: 1px solid #000;
+  border-left: 1px solid #000;
+  padding-left: 16px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const Divh1 = styled.td`
+  border-bottom: 1px solid #000;
+  padding-left: 16px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const Divh2 = styled.td`
+  border-bottom: 1px solid #000;
+  border-right: 1px solid #000;
+  padding-left: 16px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const Button = styled.button`
+  all: unset;
+  align-items: center;
+  border-radius: 100px;
+  box-sizing: border-box;
+  display: inline-flex;
+  flex-direction: column;
+  gap: 8px;
+  height: 40px;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+  border: 1px solid;
+  background-color: var(--m-3syslightprimary);
+  box-shadow: var(--m3-elevation-light-1);
+  padding: 10px 18px 7px 16px;
+  background: #CECCCC;
+
+  &:hover {
+    background: #A3A3A3; // 마우스를 올렸을 때의 배경색을 설정
+  }
+`;
+
+
 
 const Item = ({ data }) => {
-  console.log(data);
-  const navigate = useNavigate();
+
+  function Room(roommateNum){
+    if(roommateNum == "room4") return "4인실"
+    if(roommateNum == "room2") return "2인실"
+    if(roommateNum == "room1") return "1인실"
+  }
+  
+  console.log(data)
+  const navigate = useNavigate()
   return (
     <tr>
-      <td>{data.name}</td>
-      <td>{data.stuNum}</td>
-      <td>{data.team}</td>
-      <td>{data.major}</td>
-      <td>{data.birth}</td>
-      <td>{data.roommateNum}</td>
-      <td>
-        {
-          <button
-            onClick={() => {
-              console.log(data.id);
-              navigate(data.id, { state: data });
-            }}
-          >
-            상세정보
-          </button>
-        }
-      </td>
-    </tr>
-  );
-};
+        <Divh>{data.name}</Divh>
+        <Divh1>{data.stuNum}</Divh1>
+        <Divh1>{data.team}</Divh1>
+        <Divh1>{data.major}</Divh1>
+        <Divh1>{data.birth}</Divh1>
+        <Divh1>{Room(data.roommateNum)}</Divh1>
+        <Divh2>
+          {
+            <Button
+              onClick={() => {
+                console.log(data.id)
+                navigate(data.id, { state: data })
+              }}
+            >
+              상세정보
+            </Button>
+          }
+        </Divh2>
+  </tr>
+  )
+}
 
 export default Item;
